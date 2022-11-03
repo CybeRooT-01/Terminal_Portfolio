da75a5914f82d663681bed33ec757815890828a9
CybeRooT-01 <birahim267@gmail.com>
2022-11-03 03:28:35 +0000

    teminal portfolio

---
+++ Js/command.js
@@ -0,0 +1,61 @@
+var twitter = "mon twitter";
+var linkedin = "Mon linkedin";
+var instagram = "Mon insta";
+var github = "Mon github";
+var email = "mailto:birahim267@gmail.com";
+
+whoami = [
+  "<br>",
+  "Bon bref pour le moment dites vous que je suis birahim gueye",
+  "<br>",
+];
+social = [
+  "<br>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="_blank">Mon Twitter' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">Mon linkedin' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">Mon insta' +
+    "</a>",
+  'github         <a href="' + github + '" target="_blank">mon github' + "</a>",
+  "<br>",
+];
+projects = ["<br>", "hehe pas encore de projets.", "<br>"];
+
+help = [
+  "<br>",
+  '<span class="command">whoami</span>          Qui suis-je?',
+  '<span class="command">social</span>          Mes reseauax sociaux',
+  '<span class="command">projets</span>         Mes projets',
+  '<span class="command">email</span>           Mon Adress Mail',
+  '<span class="command">clear</span>           Nettoyer le terminal',
+  '<span class="command">cv</span>              Telecharger mon cv',
+  '<span class="command">banner</span>          Afficher Le header',
+  "<br>",
+];
+
+banner = [
+  "                                                                               ",
+  "  88888888ba   00                          88           00                      ",
+  "  88      88b  00                          88           00                     ",
+  "  88      ,8P                              88                                   ",
+  "  88aaaaaa8P'  88  8b,dPPYba,  ,adPPYYba,  88,dPPYba,   88  88,dPYba,,adPYba8a  ",
+  "  88888888b,   88  88P    8Y8  88     `Y8  88P'     8a  88  88P     88      8a ",
+  "  88      `88  88  88          ,adPPPPP88  88       88  88  88      88      88  ",
+  "  88      a8P  88  88          88,    ,88  88       88  88  88      88      88  ",
+  "  88888888P    88  88          ` 8bbdP8Y8  88       88  88  88      88      88 Alias CybeRooT ",
+  "                                                                                ",
+  "                                                                                   ",
+
+  '<span class="color2">Bienvenu dans Mon Portfilio en Terminal. Cliquez <a href ="../Normal/index.html" target="_blank" > >..ici </a> pour aller dans mon Portfolio Normal</span>',
+  '<span class="color2">Pour la liste des commandes, tapez</span> <span class="command">\'aide\'</span><span class="color2">.</span>',
+];
+cv = [
+  "Cliquez <a href='#' download>ici</a> pour telecharger mon Curriculum Vitae",
+];
